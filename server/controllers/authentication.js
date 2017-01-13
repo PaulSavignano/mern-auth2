5af46b8172d72d4e0bba2617553efc3fecdf583a
Paul Savignano <Paul.Savignano@gmail.com>
2017-01-12 16:50:45 -0800

    Generate jwt token encoded with secret.

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -20,9 +27,7 @@ exports.signup = function(req, res, next) {
     });
     user.save((err) => {
       if (err) { return next(err); }
-      res.json({ message: 'User saved!'});
+      res.json({ token: tokenForUser(user) });
     });
   });
-
-
 }
